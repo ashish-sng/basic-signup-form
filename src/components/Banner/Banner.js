661fd5ce31d9af6d8f0d995e4267fbefd1142dcf
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-21 14:18:21 +0530

    added banner image

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner__logo">
+        <span>Altitude Air</span>
+        <hr />
+        <p>
+          We promise to ensure that your well-being is taken care of while
+          travelling with us. Boasting top in class fleet inventory and a 5 star
+          approval for our in-flight experience, you know you're getting the
+          best from Altitute with no attitude
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
